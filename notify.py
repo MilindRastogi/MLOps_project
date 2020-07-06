efbb2b825d51dbf4d6b0532dc915ce6e8d83f4c0
Milind Rastogi <milind.189303058@muj.manipal.edu>
2020-07-06 22:22:05 +0530

    error solved

--- notify.py
+++ notify.py
@@ -2,7 +2,7 @@ import smtplib
 f = open("accuracy.txt" , 'r')
 acc = f.read()
 
-s = smtplib.SMTP(host= 'smtp.gmail.com', port= 587)
+s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
 s.login('rashmishrma660@gmail.com' , 'lastsupreme24')
 msg = 'Congratulations your accurracy is  {}'.format(acc)
